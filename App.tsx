88f954ad19f2d35ccafc8b379e65d8a20e380409
César Willemberg <cesar.willemberg@unijui.edu.br>
2025-02-14 15:04:17 -0300

    First Commit :tada: :rocket:

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import Routes from "./src/routes";
+
+import { ThemeProvider } from "styled-components/native";
+import { myTheme } from "./src/styles";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <ThemeProvider theme={myTheme}>
+        <Routes />
+      </ThemeProvider>
+    </>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+type PropsNavigationStack = {
+  Home: undefined;
+};
+
+export type PropsStack = NativeStackNavigationProp<PropsNavigationStack>;
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
